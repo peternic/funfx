6dfa35b43f8cf3ec40eab897fc2cd5b77ea1a2c4
Aslak Hellesoy <aslak.hellesoy@gmail.com>
2008-09-01 16:18:22 +0200

    Kill warnings

--- gem/spec/spec_helper.rb
+++ gem/spec/spec_helper.rb
@@ -6,16 +6,19 @@ rescue LoadError
   require 'spec'
 end
 
-$:.unshift(File.dirname(__FILE__) + '/../lib')
-case PLATFORM
-when /darwin/
-  require 'funfx/safari'
-  Watir::Browser = Watir::Safari
-when /win32|mingw/
-  require 'funfx/watir'
-  Watir::Browser = Watir::IE
-else
-  raise "This platform is not supported (#{PLATFORM})"
-end
+unless defined?(Watir::Browser)
+  $:.unshift(File.dirname(__FILE__) + '/../lib')
+  case PLATFORM
+  when /darwin/
+    require 'funfx/safari'
+    Watir::Browser = Watir::Safari
+  when /win32|mingw/
+    puts caller.inspect
+    require 'funfx/watir'
+    Watir::Browser = Watir::IE
+  else
+    raise "This platform is not supported (#{PLATFORM})"
+  end
 
-DEMO_APP = "http://localhost:9851/DemoApp.html"
+  DEMO_APP = "http://localhost:9851/DemoApp.html"
+end
