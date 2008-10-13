55e8078748506682d4db05da2de3177484e787ce
Peter Nicolai Motzfeldt <motzfeldt@gmail.com>
2008-10-13 22:10:07 +0200

    Changed the sdk version back after some testing

--- demo-app/build.bat
+++ demo-app/build.bat
@@ -1,2 +1,2 @@
-IF NOT EXIST "%FLEX_SDK_HOME%" SET FLEX_SDK_HOME=C:\Program Files\Adobe\Flex Builder 3 Plug-in\sdks\2.0.1
+IF NOT EXIST "%FLEX_SDK_HOME%" SET FLEX_SDK_HOME=C:\Program Files\Adobe\Flex Builder 3 Plug-in\sdks\3.0.0
 "%FLEX_SDK_HOME%\bin\mxmlc" -verbose-stacktraces -include-libraries ../gem/ext/FunFX.swc "%FLEX_SDK_HOME%/frameworks/libs/automation.swc" "%FLEX_SDK_HOME%/frameworks/libs/automation_dmv.swc" "%FLEX_SDK_HOME%/frameworks/libs/automation_agent.swc" -output ..\gem\website\demo-app\DemoApp.swf -- src\FlexObjectTest.mxml
