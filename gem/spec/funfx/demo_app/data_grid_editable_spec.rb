d1aca0aeea29836f80c0fd8116961626758d6aa6
Peter Nicolai Motzfeldt <motzfeldt@gmail.com>
2008-11-30 20:09:35 +0100

    Added a spec for editing a datagrid with an itemrenderer (not default way of edit a cell in a datagrid).

--- gem/spec/funfx/demo_app/data_grid_editable_spec.rb
+++ gem/spec/funfx/demo_app/data_grid_editable_spec.rb
@@ -14,43 +14,64 @@ describe "EditableDataGrid" do
 
   it "should edit a cell in the datagrid by using automationIndex" do
     @data_grid.values(0,3).should == [
-      ["Christina Coenraets", "555-219-2270", "ccoenraets@fictitious.com"],
-      ["Joanne Wall",         "555-219-2012", "jwall@fictitious.com"     ], 
-      ["Maurice Smith",       "555-219-2012", "maurice@fictitious.com"   ],
-      ["Mary Jones",          "555-219-2000", "mjones@fictitious.com"    ]
+      ["Christina Coenraets", "555-219-2270", "ccoenraets@fictitious.com", "true" ],
+      ["Joanne Wall",         "555-219-2012", "jwall@fictitious.com"     , "true" ], 
+      ["Maurice Smith",       "555-219-2012", "maurice@fictitious.com"   , "false"],
+      ["Mary Jones",          "555-219-2000", "mjones@fictitious.com"    , "true" ]
     ]
     
-    @data_grid.select("*Joanne Wall* | 555-219-2012 | jwall@fictitious.com")
-      @data_grid.text_area({:automationIndex => 'name:1'}).input('Peter')
-      @data_grid.change_focus('TAB')
+    @data_grid.select("*Joanne Wall* | 555-219-2012 | jwall@fictitious.com | true")
+    @data_grid.text_area({:automationIndex => 'name:1'}).input('Peter')
+    @data_grid.select("Peter | *555-219-2012* | jwall@fictitious.com | true")
+    @data_grid.text_area({:automationIndex => 'phone:1'}).input('7777')
+    @flex.box({:id => 'dgPanel'}).click
     
-      @data_grid.values(0,3).should == [
-        ["Christina Coenraets", "555-219-2270", "ccoenraets@fictitious.com"],
-        ["Peter",               "555-219-2012", "jwall@fictitious.com"     ], 
-        ["Maurice Smith",       "555-219-2012", "maurice@fictitious.com"   ],
-        ["Mary Jones",          "555-219-2000", "mjones@fictitious.com"    ]
-      ]
+    @data_grid.values(0,3).should == [
+      ["Christina Coenraets", "555-219-2270", "ccoenraets@fictitious.com", "true" ],
+      ["Peter",               "7777", "jwall@fictitious.com"     , "true" ], 
+      ["Maurice Smith",       "555-219-2012", "maurice@fictitious.com"   , "false"],
+      ["Mary Jones",          "555-219-2000", "mjones@fictitious.com"    , "true" ]
+    ]
+  end
+  
+  it "should edit a cell with itemrenderer in the datagrid by using automationIndex" do
+    @data_grid.values(0,3).should == [
+      ["Christina Coenraets", "555-219-2270", "ccoenraets@fictitious.com", "true" ],
+      ["Joanne Wall",         "555-219-2012", "jwall@fictitious.com"     , "true" ], 
+      ["Maurice Smith",       "555-219-2012", "maurice@fictitious.com"   , "false"],
+      ["Mary Jones",          "555-219-2000", "mjones@fictitious.com"    , "true" ]
+    ]
     
+    @data_grid.select("Joanne Wall | 555-219-2012 | jwall@fictitious.com | *true*")
+    @data_grid.combo_box({:automationIndex => 'active:1'}).open
+    @data_grid.combo_box({:automationIndex => 'active:1'}).select('false')
+    @flex.box({:id => 'dgPanel'}).click
     
+    @data_grid.values(0,3).should == [
+      ["Christina Coenraets", "555-219-2270", "ccoenraets@fictitious.com", "true" ],
+      ["Joanne Wall",         "555-219-2012", "jwall@fictitious.com"     , "false"], 
+      ["Maurice Smith",       "555-219-2012", "maurice@fictitious.com"   , "false"],
+      ["Mary Jones",          "555-219-2000", "mjones@fictitious.com"    , "true" ]
+    ]
   end
   
   it "should edit a cell in the datagrid by using automationValue" do
     @data_grid.values(0,3).should == [
-      ["Christina Coenraets", "555-219-2270", "ccoenraets@fictitious.com"],
-      ["Joanne Wall",         "555-219-2012", "jwall@fictitious.com"     ], 
-      ["Maurice Smith",       "555-219-2012", "maurice@fictitious.com"   ],
-      ["Mary Jones",          "555-219-2000", "mjones@fictitious.com"    ]
+      ["Christina Coenraets", "555-219-2270", "ccoenraets@fictitious.com", "true" ],
+      ["Joanne Wall",         "555-219-2012", "jwall@fictitious.com"     , "true" ], 
+      ["Maurice Smith",       "555-219-2012", "maurice@fictitious.com"   , "false"],
+      ["Mary Jones",          "555-219-2000", "mjones@fictitious.com"    , "true" ]
     ]
     
-    @data_grid.select("*Joanne Wall* | 555-219-2012 | jwall@fictitious.com")
+    @data_grid.select("*Joanne Wall* | 555-219-2012 | jwall@fictitious.com | true")
     @data_grid.text_area({:automationValue => 'Joanne Wall'}).input('Peter')
-    @data_grid.change_focus('TAB')
+    @flex.box({:id => 'dgPanel'}).click
     
     @data_grid.values(0,3).should == [
-      ["Christina Coenraets", "555-219-2270", "ccoenraets@fictitious.com"],
-      ["Peter",               "555-219-2012", "jwall@fictitious.com"     ], 
-      ["Maurice Smith",       "555-219-2012", "maurice@fictitious.com"   ],
-      ["Mary Jones",          "555-219-2000", "mjones@fictitious.com"    ]
+      ["Christina Coenraets", "555-219-2270", "ccoenraets@fictitious.com", "true" ],
+      ["Peter",               "555-219-2012", "jwall@fictitious.com"     , "true" ], 
+      ["Maurice Smith",       "555-219-2012", "maurice@fictitious.com"   , "false"],
+      ["Mary Jones",          "555-219-2000", "mjones@fictitious.com"    , "true" ]
     ] 
   end
   
