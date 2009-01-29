b4cffc5d692f43ca88a87e2c604287cd69ff178a
Dan Gilbert <gilbertd@businesslogic.com>
2009-01-30 02:25:34 +0800

    Added an extension to Firewatir::Firefox so that you can redirect the output of firefox to /dev/null, avoiding getting spurious firefox debugging information in your console (and the dredded "Debugger was called!).
    Signed-off-by: Peter Nicolai Motzfeldt <motzfeldt@gmail.com>

--- gem/lib/funfx/browser/firewatir.rb
+++ gem/lib/funfx/browser/firewatir.rb
@@ -5,6 +5,20 @@ require 'funfx'
 module FireWatir
   # FireWatir extension for FunFX. Allows lookup of FlexApp objects via FunFX::Browser::FlexAppLookup#flex_app.
   class Firefox
+    alias_method :initialize_without_output_redirect, :initialize
+
+    # Monkey patching initialize so that we can turn off Firefox debugging info (including the Adobe "Debugger was called" error).
+    # This is a hack and depends on firewatir passing the profile option last: system("... -jssh #{profile_opt}").
+    def initialize(options = {})
+      opts = options.dup
+      if options[:redirect_output]
+        profile_info = options[:profile] || ''
+
+        opts.merge!(:profile => "#{profile_info} >& /dev/null")
+      end
+      initialize_without_output_redirect(opts)
+    end
+
     include FunFX::Browser::FlexAppLookup
     def platform_flex_app(dom_id, app_name) #:nodoc:
       sleep(2) # TODO: Try to remove this
